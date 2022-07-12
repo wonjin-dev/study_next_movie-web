fbf217a6d083f808ab776d53f18c4f075f6a8767
wonjin-dev <fsdjin159@gmail.com>
2022-07-13 02:31:44 +0900

    clone movie card layout css

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,27 @@ const Landing: NextPage<Props> = (props) => {
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </>
   );
 };
