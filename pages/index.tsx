2bd8843dd04302607b3750af5e446365d9c706ec
wonjin-dev <fsdjin159@gmail.com>
2022-07-13 03:16:34 +0900

    #2.4 Recap

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Landing: NextPage<Props> = (props) => {
 
       {props.results.map((movie: IMovie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -32,6 +32,9 @@ const Landing: NextPage<Props> = (props) => {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
